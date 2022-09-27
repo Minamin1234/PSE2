f85e8a79d06ceb43b26913101df070df5e71cfc5
Minamin1234 <minamin37345@gmail.com>
2022-09-27 11:00:18 +0900

    課題の作成

--- 02/rensyu01.py
+++ 02/rensyu01.py
@@ -1,8 +1,30 @@
+from cmath import rect
+from operator import mod
 from turtle import *
 shape("turtle")
 col = ["orange","limegreen","gold","plum","tomato"]
-for i in range(5):
-    color(col[i])
-    circle(100)
-    left(72)
+circles = 50
+radius = 150
+drad = radius / circles
+dy = 10
+
+up()
+left(-90)
+forward(200)
+left(90)
+left(180)
+forward(200)
+left(-180)
+for i in range(circles):
+    color(col[i%5])
+    r = radius - (drad * (i+1))
+    forward(dy)
+    speed(10)
+    down()
+    circle(r)
+    up()
+    left(90)
+    forward(drad*2)
+    left(-90)
+    ##left(72)
 done()
