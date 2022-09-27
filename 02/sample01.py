7436ef73b1e66540688357c0d7eeac78b0b48a33
Minamin1234 <minamin37345@gmail.com>
2022-09-27 00:37:50 +0000

    サンプル問題の作成

---
+++ 02/sample01.py
@@ -0,0 +1,3 @@
+import random
+kuzi = ["大吉","中吉","小吉","凶"]
+print(random.choice(kuzi))
---
+++ 02/sample02.py
@@ -0,0 +1,4 @@
+h = float(input("身長は何cmですか？")) / 100
+w = float(input("体重は何kgですか？"))
+bmi = w / (h * h);
+print(f"あなたのBMI値は、{bmi}です。")
