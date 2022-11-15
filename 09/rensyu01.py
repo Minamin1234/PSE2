53312f319427dc3c4a7bdc630b9e0d2533b7fe79
Minamin1234 <minamin37345@gmail.com>
2022-11-15 11:17:25 +0900

    課題

--- 09/rensyu01.py
+++ 09/rensyu01.py
@@ -1,32 +1,43 @@
 # HT21A099 南　李玖
 # スプライトの画像や音を変更する
 from pse2pgzrun import *
+import random
 
-alien = Actor("alien")
-alien.topright = 0,10
+CATPIC_NORM = "cat01_dash"
+CATPIC_ANGRY = "cat01_angry"
+CATPIC_HAPPY = "cat01_happy"
 
-WIDTH = 500
-HEIGHT = alien.height + 20
+cat = Actor(CATPIC_NORM)
+cat.topright = 0,10
+
+WIDTH = 750
+HEIGHT = cat.height + 20
 
 def draw():
     screen.clear()
-    alien.draw()
+    screen.fill((255,255,255))
+    cat.draw()
 
 def update():
-    alien.left += 2
-    if alien.left > WIDTH:
-        alien.right = 0
+    cat.left += 4
+    if cat.left > WIDTH:
+        cat.left = 0
 
-def set_alien_normal():
-    alien.image = "alien"
+def set_cat_normal():
+    cat.image = CATPIC_NORM
+    
 
-def set_alien_hurt():
-    alien.image = "alien_hurt"
+def set_cat_touched():
+    num = random.random()
+    if(num >= 0.5):
+        cat.image = CATPIC_HAPPY
+    else:
+        cat.image = CATPIC_ANGRY
     sounds.eep.play()
-    clock.schedule_unique(set_alien_normal,0.5)
+    clock.schedule_unique(set_cat_normal,0.5)
 
 def on_mouse_down(pos):
-    if alien.collidepoint(pos):
-        set_alien_hurt()
+    if cat.collidepoint(pos):
+        set_cat_touched()
 
 pgzrun.go()
